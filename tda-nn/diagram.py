1. Added graph geodesic and scale normalized L2 metric options for diagram computation. 2. Fixed dimension issue in landscape averaging.

tda-nn/diagram.py
@@ -1,24 +1,62 @@
 import warnings
 from ripser import Rips
+import numpy as np
 
-def compute_diagram(data, rips):
+# some useful computational geometry tools
+from sklearn.neighbors import NearestNeighbors
+import sklearn.utils.graph_shortest_path as gp
+import scipy.spatial
+
+
+def compute_diagram(data, rips, metric='L2', k=10):
     # compute and return diagram
     data_cpu = data.detach().cpu().numpy()
     samples = data_cpu.reshape(data.shape[0], -1)
+
     with warnings.catch_warnings():
         # supress ripser warnings
         warnings.simplefilter("ignore")
-        pd = rips.fit_transform(samples)
+        if metric == 'L2':
+            pd = rips.fit_transform(samples)
+        elif metric == 'GG' or metric == 'graph geodesic':
+            graph_geodesic_dm = graph_geodesic_metric(samples, k)
+            pd = rips.fit_transform(graph_geodesic_dm, distance_matrix=True)
+        elif metric == 'SN' or metric == 'scale normalized':
+            normalized_data = scale_normalize(samples)
+            pd = rips.fit_transform(samples)
+        else:
+            print("Error: Unknown metric: ".format(metric))
+            quit()
 
     return pd
 
-def compute_diagram_n(model, data, rips, n):    
+def compute_diagram_n(model, data, rips, n, metric):    
     xn = model(data, n)
-    return compute_diagram(xn, rips)
+    return compute_diagram(xn, rips, metric=metric)
 
 def compute_diagram_all(model, data, rips):
     activations = model(data)
     diagrams = [compute_diagram(activation, rips) for activation in activations]
     return diagrams
+
+def graph_geodesic_metric(data, k=5):
+    '''
+    Input: Point Cloud - [N, d]
+    Output: Graph geodesic distance matrix: [N, N]
+    '''
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(data)
+    adjacency_matrix = nbrs.kneighbors_graph(data).toarray()
+    distance_matrix = gp.graph_shortest_path(adjacency_matrix, method='auto', directed=False)
+    print(distance_matrix)
+    return distance_matrix
+
+
+def scale_normalize(data, p=2):
+    distance_matrix = scipy.spatial.distance_matrix(data, data, p)
+    mean_distance = np.mean(distance_matrix)
+    normalized_data = data/mean_distance
+    return normalized_data
+
+
 
 

tda-nn/landscape.py
@@ -15,12 +15,12 @@ def landscape(diagram, dx=0.1, min_x= 0, max_x=10, threshold=-1):
     """
     if diagram.shape[0] == 0:
         print("WARNING: Empty diagram detected.")
-        return np.zeros([1, math.ceil((max_x-min_x)/dx), 2])
+        return np.zeros([1, math.floor((max_x-min_x)/dx)+1, 2])
     return np.array(landscape.tdatools.landscape_discrete(diagram, dx, min_x, max_x, threshold))
 
 landscape.tdatools = importr('tdatools')
 
-def landscapes_diagrams_from_model(net, data, maxdims, thresholds, ns, dx, min_x, max_x, id=None, mode='normal'):
+def landscapes_diagrams_from_model(net, data, maxdims, thresholds, ns, dx, min_x, max_x, id=None, mode='normal', pd_metric='L2'):
     landscapes = []
     diagrams = []
 
@@ -34,7 +34,7 @@ def landscapes_diagrams_from_model(net, data, maxdims, thresholds, ns, dx, min_x
                     thresh=threshold,
                     verbose=False)
 
-        diagrams_all = compute_diagram_n(net, data, rips, n)
+        diagrams_all = compute_diagram_n(net, data, rips, n, metric=pd_metric)
         diagrams.append(diagrams_all)
         
         def one_x_axies(landscape):