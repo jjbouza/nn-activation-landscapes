5ab26b0ed5c1b853a0c7f5648398efd7a8ce8776
Jose Bouza <josebouza@login4.ufhpc>
2020-09-25 13:03:54 -0400

    Added 9_25_reproduce to attempt to reproduce row 6 of table 1 in OG paper.

---
+++ experiments/9_25_reproduce/model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ripser import Rips
+
+class Net(nn.Module):
+    """
+    MLP for simple classification task.
+    """
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc_first = nn.Linear(2, 15)
+        self.middle_fc = [nn.Linear(15, 15) for _ in range(9)]
+        self.fc_last = nn.Linear(15, 2)
+
+        self.fc_layers = [self.fc_first]+self.middle_fc+[self.fc_last]
+        self.layers = nn.ModuleList([nn.Sequential(fc_layer, nn.ReLU()) for fc_layer in self.fc_layers[:-1]])
+
+
+    def forward(self, x, n=None):
+        """
+        Net.forward(x, n) means return the nth layer activation of Net given input x.
+        If instead Net.forward(x) is called then a list with all of the hidden layer 
+        activations is returned.
+
+        NOTE: TO WORK WITH TDA-NN THE FORWARD FUNCTION OF YOUR MODEL MUST ACCEPT THE 'n' PARAMATER.
+        """
+        if n == 'all':
+            activation_list = [x]
+            for f in self.layers[:n]:
+                activation_list.append(f(activation_list[-1]))
+            return activation_list
+
+        if n == None:
+            n = len(self.layers)
+
+        for f in self.layers[:n]:
+            x = f(x)
+
+        return x
+
