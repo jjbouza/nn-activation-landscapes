d1ea368fd7de792148d3ab028f3ed2b8e1506b41
Jose Bouza <josejbouza@gmail.com>
2020-11-06 15:45:19 -0500

    Network training CLI.

--- experiments/1015_graph_geodesic/train.sh
+++ experiments/1015_graph_geodesic/train.sh
@@ -1,8 +1,15 @@
-PYTHONPATH=./:../../tda-nn/ python3 trainer.py \
-    --model model \
-    --csv-file disk6.csv \
-    --training-threshold 0.8 \
-    --batch-size 2560 \
-    --max-epochs 8000 \
-    --learning-rate 0.01 \
-    --output-name output/network
+OUTPUT_FOLDER=$2
+NETWORK_COUNT=$1
+
+for net_id in $(seq 1 $NETWORK_COUNT);
+do
+    PYTHONPATH=./:../../tda-nn/ python3 trainer.py \
+        --model model \
+        --csv-file disk6.csv \
+        --training-threshold 0.99 \
+        --batch-size 2560 \
+        --max-epochs 8000 \
+        --learning-rate 0.01 \
+        --output-name $OUTPUT_FOLDER/network${net_id} \
+        ;
+done
