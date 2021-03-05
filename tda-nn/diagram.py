1. Fixed SN metric normalization. 2. Fixed non-plotting of diagram if only one of the diagrams was empty.

tda-nn/diagram.py
@@ -61,7 +61,7 @@ def compute_diagram(data, maxdim, threshold, metric='L2', k=12, save_activations
             normalized_X = scale_normalize(X)
             pd = ripser(normalized_X, maxdim, threshold)['dgms']
             if save_activations_plots is not None:
-                visualize.plot_activations(X, None, save=save_activations_plots)
+                visualize.plot_activations(normalized_X, None, save=save_activations_plots)
         else:
             error("Error: Unknown metric: ".format(metric))
             quit()
@@ -85,12 +85,15 @@ def graph_geodesic_metric(adjacency_matrix):
     return distance_matrix
 
 def scale_normalize(data, p=2):
-    distance_matrix = scipy.spatial.distance_matrix(data, data, p)
-    mean_distance = np.mean(distance_matrix)
-    if mean_distance == 0:
-        normalized_data = data
+    translated_data = data-np.mean(data, axis=0)
+    distance_matrix = scipy.spatial.distance_matrix(translated_data, translated_data, p)
+
+    max_distance = np.max(distance_matrix)
+    if max_distance == 0:
+        normalized_data = translated_data
     else:
-        normalized_data = data/mean_distance
+        normalized_data = translated_data/max_distance
+
     return normalized_data
 
 def save_diagram(diagram, dirname):

tda-nn/visualize.py
@@ -7,7 +7,8 @@ from matplotlib import pyplot as plt
 from persim import visuals, plot_diagrams
 
 from sklearn.decomposition import PCA
-from utils import int2str_with_leading_zero
+from utils import int2str_with_leading_zero, warning
+import scipy.spatial
 
 def indices(arr):
     if len(arr.shape) == 0:
@@ -30,10 +31,14 @@ def save_diagram_plots(diagrams, dirname):
 
     fig, ax = plt.subplots()  # Create a figure and an axes.
     for layer, diagram in enumerate(diagrams):
-        plot_diagrams(diagram, show=False, ax=ax)
-        layer_id = int2str_with_leading_zero(layer, len(diagrams))
-        fig.savefig(os.path.join(dirname, 'layer{}.png'.format(layer)))
-        plt.cla()
+        plot_only = [d for d in diagram if d.shape[0] != 0]
+        if any([d.shape[0] == 0 for d in diagram]):
+            warning("Not plotting some diagrams with 0 entries.")
+        if plot_only != []:
+            plot_diagrams(plot_only, show=False, ax=ax)
+            layer_id = int2str_with_leading_zero(layer, len(plot_only))
+            fig.savefig(os.path.join(dirname, 'layer{}.png'.format(layer)))
+            plt.cla()
     plt.close('all')
 
 def save_landscape_plots(landscapes, dirname):
@@ -73,6 +78,8 @@ def plot_activations(data, adjacency_matrix, save=None):
     plt.clf()
     pca = PCA(n_components=2)
     new_data = pca.fit_transform(data)
+    distance_matrix = scipy.spatial.distance_matrix(new_data, new_data, 2)
+
     plt.scatter(new_data[:,0], new_data[:,1])
     if adjacency_matrix is not None:
         for i in range(adjacency_matrix.shape[0]):