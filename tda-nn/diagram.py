76474b3e1139874f421346725c346a3a2e4d78fb
Jose Bouza <josejbouza@gmail.com>
2021-10-05 16:50:07 -0400

    diagram.py Supports per-layer centering and percentiles for new percentile normalization.

--- tda-nn/diagram.py
+++ tda-nn/diagram.py
@@ -15,15 +15,16 @@ import resource
 
 from utils import *
 
-def compute_diagrams(data, maxdims, thresholds, metric='L2', k=12, percentile=0.9, centers=None, save_activations_plots=None):
+def compute_diagrams(data, maxdims, thresholds, metric='L2', k=12, percentiles=None, centers=None, save_activations_plots=None):
     if save_activations_plots is not None:
         if not os.path.exists(save_activations_plots):
             os.makedirs(save_activations_plots)
 
     center_list = [None for _ in maxdims] if centers is None else centers
+    percentile_list = [None for _ in maxdims] if percentiles is None else percentiles
 
     diagrams = []
-    for id, (activation, dim, threshold, center) in enumerate(zip(data, maxdims, thresholds, center_list)):
+    for id, (activation, dim, threshold, center, percentile) in enumerate(zip(data, maxdims, thresholds, center_list, percentile_list)):
         act_plot = None if save_activations_plots is None else os.path.join(save_activations_plots, 'layer{}.png'.format(id))
         diag = compute_diagram(activation, 
                                dim, 
@@ -79,7 +80,9 @@ def compute_diagram(data, maxdim, threshold, metric='L2', k=12, percentile=0.9, 
             normalized_data, percentile_index= percentile_normalize(X, percentile, center)
             distance_matrix = scipy.spatial.distance_matrix(normalized_data, normalized_data)
             # Because we sorted the data, we just need to set the block beyond row and col = percentile_index to 0.
-            distance_matrix[percentile_index:, percentile_index:] = 0
+            if percentile_index < distance_matrix.shape[0]:
+                distance_matrix[percentile_index:, percentile_index:] = 0
+
             pd = ripser_parallel(distance_matrix, maxdim=maxdim, thresh=threshold, metric='precomputed', n_threads=-1)['dgms']
             if save_activations_plots is not None:
                 visualize.plot_activations(X, None, save=save_activations_plots)
@@ -134,8 +137,7 @@ def percentile_normalize(data, percentile, center=None, p=2):
     distances = distances[argsort]
 
     percentile_index = int(distances.shape[0]*percentile) # index in translated_data where "outlier" region begins. 
-
-    normalized_data = data/distances[percentile_index]
+    normalized_data = data/distances[min(percentile_index, distances.shape[0]-1)]
 
     return normalized_data, percentile_index
 
@@ -171,8 +173,8 @@ if __name__ == '__main__':
     parser.add_argument('--persistence-layers', type=int, nargs='+')
     parser.add_argument('--diagram-metric', type=str)
     parser.add_argument('--nn-graph-k', type=int)
-    parser.add_argument('--center', type=int, nargs='+')
-    parser.add_argument('--percentile', type=float)
+    parser.add_argument('--center', type=int, nargs='+', default=None)
+    parser.add_argument('--percentile', type=float, nargs='+')
     parser.add_argument('--save-diagram-plots', default=None)
     parser.add_argument('--output-dir', type=str)
 
@@ -181,7 +183,7 @@ if __name__ == '__main__':
     # take first persistence-data-samples rows from activations
     data_ = load_data(args.activations)
     data = [data_[layer] for layer in args.persistence_layers]
-    center = [bool(c) for c in args.center]
+    center = [bool(c) for c in args.center] if args.center is not None else args.center
     diagrams = compute_diagrams(data, 
                                 args.max_diagram_dimension, 
                                 args.diagram_threshold, 
